65a52b1ed0b18123f03a05a373b9320a41215f84
Yuki Hayashi <mrchildreny@gmail.com>
2023-07-16 12:12:21 +0900

    Home画面設置

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Link, Route, Routes } from 'react-router-dom'
 import { UncontrolledStep1 } from './patterns/uncontrolledPattern/pages/uncontrolled/step1'
 import { UncontrolledStep2 } from './patterns/uncontrolledPattern/pages/uncontrolled/step2'
 import { ControlledStep1 } from './patterns/controlledPattern/pages/controlled/step1'
 import { ControlledStep2 } from './patterns/controlledPattern/pages/controlled/step2'
 
+const Home = () => {
+  return (
+    <ul style={{ textAlign: 'left' }}>
+      <h1>react-hook-form サンプル集</h1>
+      <li><Link to={'/uncontrolled/step1'}>Uncontrolled Form Example</Link></li>
+      <li><Link to={'/controlled/step1'}>Controlled Form Example</Link></li>
+    </ul>
+  )
+}
+
 function App() {
   return (
-    <Routes>
-      <Route path="/uncontrolled/step1" element={<UncontrolledStep1 />} />
-      <Route path="/uncontrolled/step2" element={<UncontrolledStep2 />} />
-      <Route path="/controlled/step1" element={<ControlledStep1 />} />
-      <Route path="/controlled/step2" element={<ControlledStep2 />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/uncontrolled/step1" element={<UncontrolledStep1 />} />
+        <Route path="/uncontrolled/step2" element={<UncontrolledStep2 />} />
+        <Route path="/controlled/step1" element={<ControlledStep1 />} />
+        <Route path="/controlled/step2" element={<ControlledStep2 />} />
+      </Routes>
+    </>
   )
 }
 
